1b5f5b16a9f323c7b955ae232cd5655dd24fca5c
Juan Carlos Alvarez <juan.alvarez.s@segic.usach.cl>
2021-04-08 18:03:28 -0400

    Se implementa coneccion con BD

--- code/db-basico.php
+++ code/db-basico.php
@@ -1,2 +1,14 @@
 <?php
-echo "Este es el archivo Básico BD: jc";
+
+$dbname = "registro"; //modicair por valor no valido y comprobar try
+$dbuser = "registro-user";
+$dbpassword = "registro-user1";
+
+try {$dsn = "mysql:host=localhost;dbname=$dbname";
+
+    //objeto de conexion a la bd
+    $db = new PDO($dsn, $dbuser, $dbpassword);
+}catch(PDOException $e){
+    echo $e->getMessage();
+
+}
