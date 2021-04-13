c6f1f0aaf8ac340ac36f7112807be1431822b6d5
Juan Carlos Alvarez <juan.alvarez.s@segic.usach.cl>
2021-04-13 15:50:11 -0400

    se corrige insert

--- code/db-basico.php
+++ code/db-basico.php
@@ -12,3 +12,29 @@ try {$dsn = "mysql:host=localhost;dbname=$dbname";
     echo $e->getMessage();
 
 }
+
+//preparar consulta
+$sql = "INSERT INTO users
+        (full_name, email, user_name, password)
+        VALUES
+        (:full_name, :email, :user_name, :password);";
+
+//statement
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan PErez';
+$email = 'juan.perez@segic.usach.cl';
+$user_name = 'juan.perez';
+$password = 'juan123';
+
+$stmt->bindParam(':full_name',$full_name);
+$stmt->bindParam(':email',$email);
+$stmt->bindParam(':user_name',$user_name);
+$stmt->bindParam(':password',$password);
+
+$stmt->execute();
+/*
+$id=3;
+$stmt = $db->prepare("DELETE FROM users where id=:id");
+$stmt->bindParam(':id',$id);
+$stmt->execute(); *7
