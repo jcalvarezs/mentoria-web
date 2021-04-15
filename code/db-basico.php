3aef1ec85e7878acf3f34c25b2fcddd33fd5b6f5
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-04-14 23:20:50 -0400

    Se implementa insert

--- code/db-basico.php
+++ code/db-basico.php
@@ -15,3 +15,24 @@ try {$dsn = "mysql:host=localhost;dbname=$dbname";
     echo $e->getMessage();
 
 }
+   
+// Preparar la INSERT
+$sql ="INSERT INTO users
+(full_name, email, user_name, password)
+VALUES
+(:full_name, :email, :user_name, :password)";
+
+// stament
+$stmt = $db->prepare($sql);
+
+$full_name = 'Raul Marin4';
+$email = 'raul.marin@segic.usach.cl4';
+$user_name = "raul.marin4";
+$password = password_hash("user4", PASSWORD_DEFAULT);
+
+$stmt->bindParam(':full_name', $full_name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':user_name', $user_name);
+$stmt->bindParam(':password', $password);
+
+$stmt->execute();
