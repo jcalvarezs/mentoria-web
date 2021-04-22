73849aecb8a70bd03a459a9da37621699e1e91c4
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-04-21 23:49:46 -0400

    se corrige archivo process separando array

--- code/process.php
+++ code/process.php
@@ -1,9 +1,11 @@
 <?php
 // arreglo request
 print_r($_REQUEST);
+echo "<br>";
 
 // arreglo request
 print_r($_POST);
+echo "<br>";
 
 // arreglo request
 print_r($_GET);
@@ -11,5 +13,4 @@ echo "<br>";
 
 //Arreglo SERVER
 print_r($_SERVER);
-
 echo "<br>";
