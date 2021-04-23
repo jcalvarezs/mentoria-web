9964d32941a79d450e04f96ebdb8003efa36285a
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-04-23 09:57:03 -0400

    se agrega formulario de login

--- code/index.php
+++ code/index.php
@@ -1,44 +1,3 @@
-<?php
-
-// PDO
-require "util/db.php";
-
-$valido =0;
-
-if (isset($_POST["sign-up-button"])){
-// se menvió formulario
-	$db = connectDB();
-
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$username = $_POST["username"];
-	$pass = $_POST["pass"];
-	$repeatPass = $_POST["repeat-pass"];
-	$rememberMe = $_POST["remember-me"];
-	
-$sql ="INSERT INTO users
-			(full_name, email, user_name, password)
-		VALUES
-			(:full_name, :email, :user_name, :password)";
-
-$stmt = $db->prepare($sql);
-
-    $stmt->bindParam(':full_name', $name);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':user_name', $username);
-    $password = password_hash($pass, PASSWORD_DEFAULT);
-    $stmt->bindParam(':password', $password);
-                               
-    $stmt->execute();
-
-	$message= "Registro almacenado";
-	$valido =1;
-}
-else{
-	$message= "no se ha enviado la informacion";
-	$valido =1;
-}
-
 
 
 ?>
@@ -88,24 +47,8 @@ else{
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
 				<form class="login100-form validate-form" method="POST" action="index.php">
 					<span class="login100-form-title p-b-59">
-						Sign Up
+						Sign In
 					</span>
-
-					<?php if ($valido== 1):?>
-					<p class ="msg-form" ><? $message; ?></p>
-					<?php endif; ?>
-
-					<div class="wrap-input100 validate-input" data-validate="Name is required">
-						<span class="label-input100">Full Name</span>
-						<input class="input100" type="text" name="name" placeholder="Name...">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<span class="label-input100">Email</span>
-						<input class="input100" type="text" name="email" placeholder="Email addess...">
-						<span class="focus-input100"></span>
-					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Username is required">
 						<span class="label-input100">Username</span>
@@ -119,37 +62,7 @@ else{
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
-						<span class="label-input100">Repeat Password</span>
-						<input class="input100" type="Password" name="repeat-pass" placeholder="*************">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="flex-m w-full p-b-33">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								<span class="txt1">
-									I agree to the
-									<a href="#" class="txt2 hov1">
-										Terms of User
-									</a>
-								</span>
-							</label>
-						</div>
-
-						
-					</div>
-
 					<div class="container-login100-form-btn">
-						<div class="wrap-login100-form-btn">
-							<div class="login100-form-bgbtn"></div>
-							<button class="login100-form-btn" name="sign-up-button">
-								Sign Up
-							</button>
-							<input type="hidden" name="super-secreto" value="valor secreto">
-						</div>
-
 						<a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
 							Sign in
 							<i class="fa fa-long-arrow-right m-l-5"></i>
