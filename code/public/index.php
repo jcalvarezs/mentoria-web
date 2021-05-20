f813f7650311b9846c5af013b4cb80371c0da996
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-05-20 15:37:31 -0400

    Se agrega loyaut y dir

--- code/public/index.php
+++ code/public/index.php
@@ -5,7 +5,7 @@ require_once  __DIR__ .'/../vendor/autoload.php';
 use app\core\Application;
 echo __DIR__;
 echo "<br>";
-dirname(__DIR__);
+echo dirname(__DIR__);
 $app = new Application();
 
 $app->router->get('/', 'home');
