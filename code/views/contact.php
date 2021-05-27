c2f3396b97c39e035b57d32f6070038df692963a
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-05-27 11:49:28 -0400

    Se genera formulario de prueba01

--- code/views/contact.php
+++ code/views/contact.php
@@ -1,27 +1,18 @@
-    <h1>Contact!</h1>
-    <form method="POST">
+<h1>Contact Us</h1>
+
+<form method="POST">
   <div class="mb-3">
     <label class="form-label">Subject</label>
-    <input type="text" name ="Subject"class="form-control">
+    <input type="text" name="subject" class="form-control">    
   </div>
-  </div>
-
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Email</label>
-    <input type="email" name ="email" class="form-control">
+    <label class="form-label">Email</label>
+    <input type="text" name="email" class="form-control">    
   </div>
-
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label class="form-label">Body</label>    
+    <textarea name="body" class="form-control" id="" cols="30" rows="10"></textarea>    
   </div>
-
-  <div class="mb-3">
-    <label class="form-label">Body</label>
-    <textarea name="body" id="" cols="30" rows="10"></textarea>
- 
-  </div>
-
 
   <button type="submit" class="btn btn-primary">Send</button>
 </form>
