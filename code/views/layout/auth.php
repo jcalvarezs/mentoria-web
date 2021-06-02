26b78dcbce6c65c76dfd78150753a399b4c8a1d9
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-06-02 13:57:55 -0400

    Se corrigen cambio de carpeta de login y register4

--- code/views/layout/auth.php
+++ code/views/layout/auth.php
@@ -24,7 +24,7 @@
           <a class="nav-link active" aria-current="page" href="/login">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/contact">Register</a>
+          <a class="nav-link" href="/register">Register</a>
         </li>
 
       </ul>
