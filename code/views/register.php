1553bd7bba1d9b2ed375297bee939b11cf38d582
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-06-02 13:41:58 -0400

    Se corrigen cambio de carpeta de login y register2

---
+++ code/views/register.php
@@ -0,0 +1,24 @@
+<h1>Register</h1>
+
+<form method="POST">
+  <div class="mb-3">
+    <label class="form-label">firstname</label>
+    <input type="text" name="firstname" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">lastname</label>
+    <input type="text" name="lastname" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input type="text" name="email" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Pasword</label>
+    <input type="text" name="email" class="form-control">    
+  </div>
+  <div class="mb-3">
+    <label class="form-label">corfinme pasword</label>
+    <input type="text" name="email" class="form-control">    
+  <button type="submit" class="btn btn-primary">Save</button>
+</form>
