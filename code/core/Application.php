0379071b722d40649edd13bf2545e274b1f93361
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-06-02 13:02:15 -0400

    Se corrige application

--- code/core/Application.php
+++ code/core/Application.php
@@ -7,7 +7,8 @@ class Application{
     public Router $router;
     public Request $request;
     public Response $response;
-    public Controller $controller
+    public Controller $controller;
+
     public static Application $app;
     
     public function __construct($rootpath)
