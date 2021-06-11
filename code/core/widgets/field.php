aa15f67226fa46ddeab4c4623d787e67e777826e
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-06-11 13:38:14 -0400

    Se corrige error en field2

--- code/core/widgets/field.php
+++ code/core/widgets/field.php
@@ -23,7 +23,7 @@ class field
     {
         return sprintf('
         <div class="mb-3">
-            <label class="form-label">FirstName</label>
+            <label class="form-label">%s</label>
             <input type="%s" name ="%s" value="%s" class="form-control %s">
     
             <div class="invlaid-feedback">
@@ -35,7 +35,7 @@ class field
         $this->type,
         $this->attribute,
         $this->model->{$this->attribute},
-        $this->model->hasError($this->attribute)? 'is-invalid' : ' ',
+        $this->model->hasError($this->attribute) ? 'is-invalid' : ' ',
         $this->model->getFirstError($this->attribute)
         );
     }
