4532f92c65d3fbf017efe2021e4b60d4e5039845
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-09-10 15:38:59 -0300

    Agregamos categoria4

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -1,27 +1,27 @@
 @extends('layout')
 
 @section('banner')
-    <h1>El Super Blog</h1>
+    <h1>El super blog</h1>
 @endsection
 
 @section('content')
+
     @if (count($posts) > 0)
-        @foreach($posts as $post)
+        @foreach ($posts as $post)
             <article>
                 <h1>
-                    <a href="/post/<?= $post->slug ?>">
+                    <a href="/post/<?= $post->slug?>">
                         {{ $post->title }}
-                    </a>    
+                    </a>
                 </h1>
-                <p>
-                <a href="/category/{{ $post->category->id ?>">
-                        {{ $post->category->name }}
-                    </a> 
-                    </p>      
-                <p><?= $post->resumen ?></p>
+                <a href="/category/{{$post->category->id}}">
+                    {{$post->category->name}}
+                </a>    
+                <p>{!! $post->resumen !!}</p>
             </article>
         @endforeach
     @else
-        I don´t have any posts !
-    @endif    
+        I don't have any posts!
+    @endif
+       
 @endsection
