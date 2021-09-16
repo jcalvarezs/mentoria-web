7ebb7b8b2b75d0b1b50a2f6f7bedd5d81f04c079
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-09-16 10:17:09 -0300

    Agregamos relaciones en post y user

--- mentoria/blog/database/seeders/DatabaseSeeder.php
+++ mentoria/blog/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
-    }
+        // \App\Models\User::factory(10)->create();
+        user::truncate();
+        Category::truncate();
+
+        $user=User::factory(10)->create();
+
+
+        $personal = Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal'
+         ]);     
+
+        $work = Category::create([
+            'name'=>'Work',
+            'slug'=>'work'
+        ]);
+        $hobbies = Category::create([
+            'name'=>'Hobbies',
+            'slug'=>'hobbies'  
+        ]);   
+        
+         post::create([
+            'category_id' => $work->id,
+            'user_id' => $user->id,
+            'slug' => 'my-firt-post',
+            'title' => 'My First Post',
+            'resumen' => 'The are many',
+            'body' => 'Teha are many viartions'
+        ]) ;  
+
+
+         }
+
+
+
+    
 }
