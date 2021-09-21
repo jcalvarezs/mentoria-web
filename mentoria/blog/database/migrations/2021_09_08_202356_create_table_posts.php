fb00436a9c7237fe4e8ebe654b1ca51c51d7823c
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-09-20 21:03:37 -0300

    Se agrega databaseseed2

--- mentoria/blog/database/migrations/2021_09_08_202356_create_table_posts.php
+++ mentoria/blog/database/migrations/2021_09_08_202356_create_table_posts.php
@@ -17,8 +17,8 @@ class CreateTablePosts extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
-            $table->foreignId('category_id');          
+            $table->foreignId('category_id'); 
+            $table->foreignId('user_id');         
             $table->string('slug')->unique();
             $table->string('title');
             $table->string('resumen');
