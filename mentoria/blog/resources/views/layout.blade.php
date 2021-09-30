6873b39079123a1541905bcf1df79d4bf48ee9b6
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-09-30 11:56:18 -0300

    Se carga nuevo index y carpeta imagenes

--- mentoria/blog/resources/views/layout.blade.php
+++ mentoria/blog/resources/views/layout.blade.php
@@ -488,6 +488,7 @@
                         <button type="submit"
                                 class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
                         >
+                        
                             Subscribe
                         </button>
                     </form>
