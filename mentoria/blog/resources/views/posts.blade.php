a3b9829a8be33109abdb1cd1e40cdca3d90a7ccc
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-10-06 20:32:51 -0300

    prueba de merge de la clases6

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -7,7 +7,8 @@
 
             <div class="lg:grid lg:grid-cols-6">
                 @foreach ($posts->skip(1) as $post)
-                    <x-post-card post="$post" class="bg-red-500"/>
+                @dd($loop)
+                    <x-post-card post="$post" class="bla"/>
                 @endforeach    
             </div>
         </main>
