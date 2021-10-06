366d929bf552d07ccd1589dbb70f427a974cdf11
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-10-06 18:35:02 -0300

    se agrega foreach

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -6,8 +6,9 @@
         <x-post-main :post="$posts[0]"/>
 
             <div class="lg:grid lg:grid-cols-2">
-                <x-post-card/>
-                <x-post-card/>
+                @foreach ($posts->skip(1) as $post)
+                    <x-post-card/>
+                @endforeach    
             </div>
 
             <div class="lg:grid lg:grid-cols-3">
