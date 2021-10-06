849fbbb26ba689e555ad9ddcb413ed6c006f3757
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-10-06 20:07:43 -0300

    prueba de merge de la clases2

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -5,10 +5,16 @@
         <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-main :post="$posts[0]"/>
 
-            <div class="lg:grid lg:grid-cols-6">
+            <div class="lg:grid lg:grid-cols-2">
                 @foreach ($posts->skip(1) as $post)
-                    <x-post-card :post= "$posts" class="bg-red-500" />
+                    <x-post-card/>
                 @endforeach    
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+                <x-post-card/>
+                <x-post-card/>
+                <x-post-card/>
             </div>
         </main>
 </x-layout>
