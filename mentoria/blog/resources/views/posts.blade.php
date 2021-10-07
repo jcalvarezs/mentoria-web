67803e542ea28f796da4d6bb495c7484adb49d31
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-10-06 21:25:45 -0300

    prueba de merge de la clases8

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -1,4 +1,3 @@
-
 <x-layout>
 @include('_posts-header')
         
@@ -7,8 +6,7 @@
 
             <div class="lg:grid lg:grid-cols-6">
                 @foreach ($posts->skip(1) as $post)
-                @dd($loop)
-                    <x-post-card post="$post" class="{{ $loop->iteration<3 ? 'col-span3' : 'col-span3'}}"/>
+                    <x-post-card :post="$post" class="{{ $loop->iteration<3 ? 'col-span-3' : 'col-span-2'}}"/>
                 @endforeach    
             </div>
         </main>
