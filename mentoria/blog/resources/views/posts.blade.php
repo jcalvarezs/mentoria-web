4e05e913b05e4ce2ded5fe22887f1fb60ced399a
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-10-06 22:00:35 -0300

    se agrega post blade y se corrigen imagenes

--- mentoria/blog/resources/views/posts.blade.php
+++ mentoria/blog/resources/views/posts.blade.php
@@ -1,3 +1,4 @@
+
 <x-layout>
 @include('_posts-header')
         
@@ -6,7 +7,7 @@
 
             <div class="lg:grid lg:grid-cols-6">
                 @foreach ($posts->skip(1) as $post)
-                    <x-post-card :post="$post" class="{{ $loop->iteration<3 ? 'col-span-3' : 'col-span-2'}}"/>
+                    <x-post-card post="$post" class="{{ $loop->iteration<3 ? 'col-span-3' : 'col-span-2'}}"/>
                 @endforeach    
             </div>
         </main>
