46a08754a924bbf4b151f555a4fd60d19edb9d77
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-11-09 13:32:40 -0300

    se agregan componentes dropdows y dropdows-item 16

--- mentoria/blog/resources/views/categories.blade.php
+++ mentoria/blog/resources/views/categories.blade.php
@@ -1,17 +1,15 @@
-<x-layout>
-    @include('_posts-header')
+@extends("layout")
+@section("content")
 
-    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if ($posts->count())
-            <x-category-button :post="$posts[0]" />
+<article>
+    <!-- <p> {{$post->body}} </p> -->
 
-            <div class="lg:grid lg:grid-cols-6">
-                @foreach ($posts->skip(1) as $post)
-                    <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
-                @endforeach
-            </div>
-        @else
-            <p class="text-center">No posts yet.</p>
-        @endif
-    </main>
-</x-layout>
+    <p> <a href="/category/{{$post->category->slug}}">
+            {{$post->category->name}}
+        </a>
+    </p>
+
+    <p><?= $post->resumen ?></p>
+</article>
+<a href="/">Go Back</a>
+@endsection
