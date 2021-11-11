4cf99bcda0fdb5f4fc8693c77ba036edd2d84574
JC alvarez <juan.alvarez.s@segic.usach.cl>
2021-11-11 14:56:25 -0300

    se agregan componentes dropdows y dropdows-item 20

--- mentoria/blog/app/Models/Post.php
+++ mentoria/blog/app/Models/Post.php
@@ -8,7 +8,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Post extends Model
 {
     use HasFactory;
-    public $fillable = ['title', 'resumen', 'body'];
+
+    //public $fillable = ['title', 'resumen', 'body'];
+
+    //sin proteger ninguna propiedad
+    //public $guarded = [];
+    //proteger id
+    //public $guarded = ['id'];
     protected $guarded = ['id'];
     public $with = ['category', 'author'];
 
@@ -19,21 +25,62 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-         
-        if ($filters['search'] ?? false) {       
-                  return $query->where('title', 'like', '%' . $filters['search'] . '%')
-                    ->orWhere('resumen', 'like', '%' . $filters['search'] . '%');
-        }
-        
+        //usando la forma when 
+        $query->when(
+            $filters['search'] ?? false, //corregido
+            //isset($filters['search']), //esto no va!!
+            fn ($query, $search) =>
+            $query->where('title', 'like', "%$search%")
+                    ->orWhere('resumen', 'like', "%$search%"));
+
+        // return $query->when(
+        //     $filters['category'] ?? false, //corregido
+        //     fn ($query, $category) =>
+        //         $query
+        //             ->whereExists(function($query) {
+        //             $query
+        //                 ->from('categories')
+        //                 ->whereColumn('categories.id', 'posts.category_id')
+        //         })      ->where('categories.slug', $category)
+        // );
+
+        return $query->when(
+            $filters['category'] ?? false,
+            fn ($query, $category) =>
+            $query ->WhereHas('category', fn($query) =>
+            $query ->Where('slug', $category))
+        );
+
+        //return $query;
+
+    //    if (request('search')) {
+    //     if (isset($filters['search'])) {       
+    //         //agregar las condiciones de busqueda
+    //         return $query->where('title', 'like', '%' . $filters['search'] . '%')
+    //                 ->orWhere('resumen', 'like', '%' . $filters['search'] . '%');
+    //     }
+    //     }
+
+
     }
 
-     public function category()
+
+    //relation: hasone, hasMany, belongsTo, belongsToMany
+    public function category()
     {
+        //nombre clase la primera con mayuscula
         return $this->belongsTo(Category::class);
     }   
 
+    //modifica de user a author, laravel supone que en bd el campo es author_id
+    //para que no suceda esto, belong tiene como campo opcional paso de llave foranea
+    //en este caso user_id 
+    //public function user()
     public function author()
     {
+        //Post pertenece a un usuario
+        //return $this->belongsTo(User::class);
         return $this->belongsTo(User::class, 'user_id');
     }   
+
 }
